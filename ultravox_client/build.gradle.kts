da534b228381119f0bb1066e7f989441d203b83a
Mike Depinet <mike@fixie.ai>
2024-08-27 12:17:38 -0700

    Maven publishing (#2)

--- ultravox_client/build.gradle.kts
+++ ultravox_client/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    `maven-publish`
+    signing
 }
 
 android {
@@ -9,6 +11,9 @@ android {
 
     defaultConfig {
         minSdk = 21
+        aarMetadata {
+            minCompileSdk = 21
+        }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -30,6 +35,66 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    publishing {
+        singleVariant("release") {
+            withJavadocJar()
+            withSourcesJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "ai.fixie"
+            artifactId = "ultravox-client-sdk"
+            version = "0.1.0"
+
+            pom {
+                name = "Ultravox Client"
+                description = "Android client code for joining an Ultravox call"
+                url = "https://github.com/fixie-ai/ultravox-client-sdk-android"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        name = "Mike Depinet"
+                        email = "mike@fixie.ai"
+                        organization = "Fixie.AI"
+                        organizationUrl = "https://fixie.ai"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/fixie-ai/ultravox-client-sdk-android.git"
+                    developerConnection =
+                        "scm:git:ssh://github.com:fixie-ai/ultravox-client-sdk-android.git"
+                    url = "https://github.com/fixie-ai/ultravox-client-sdk-android/tree/main"
+                }
+            }
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+    // To publish, uncomment this block and set the file url appropriately.
+    // You'll also need to set signing.{keyId,password,secretKeyRingFile} in
+    // gradle.properties after creating, uploading, and exporting a gpg key.
+//    repositories {
+//        maven {
+//            name = "LocalForCentralUpload"
+//            url = uri("file:///home/mike/tmp/maven")
+//        }
+//    }
+}
+
+signing {
+    sign(publishing.publications["release"])
 }
 
 dependencies {
